00b4ce1a66dcd64a6425e3ab8f5ab303c70b516b
Annontations6 <106895624+Annontations6@users.noreply.github.com>
2022-06-11 13:13:32 -0300

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -109,9 +109,9 @@ addLayer("γ", {
 		points: new Decimal(0),
     }},
     color: "#808080",
-    requires: new Decimal(10000), // Can be a function that takes requirement increases into account
-    resource: "beta", // Name of prestige currency
-    baseResource: "aplha", // Name of resource prestige is based on
+    requires: new Decimal(1e9), // Can be a function that takes requirement increases into account
+    resource: "gamma", // Name of prestige currency
+    baseResource: "beta", // Name of resource prestige is based on
     baseAmount() {return player.points}, // Get the current amount of baseResource
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
     exponent: 0.5, // Prestige currency exponent
