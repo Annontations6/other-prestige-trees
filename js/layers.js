22c0ca489121a32cde284aeaf4f4ededefde7485
Annontations6 <106895624+Annontations6@users.noreply.github.com>
2022-06-11 13:07:50 -0300

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -92,5 +92,51 @@ addLayer("β", {
     		description: "9x10",
     		cost: new Decimal(69),
         },
+		13: {
+			title: "x10 gain",
+    		description: "9x10x10",
+    		cost: new Decimal(69),
+        },
+    },
+})
+
+addLayer("γ", {
+    name: "gamma", // This is optional, only used in a few places, If absent it just uses the layer id.
+    symbol: "γ", // This appears on the layer's node. Default is the id with the first letter capitalized
+    position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
+    startData() { return {
+        unlocked: true,
+		points: new Decimal(0),
+    }},
+    color: "#808080",
+    requires: new Decimal(10000), // Can be a function that takes requirement increases into account
+    resource: "beta", // Name of prestige currency
+    baseResource: "aplha", // Name of resource prestige is based on
+    baseAmount() {return player.points}, // Get the current amount of baseResource
+    type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
+    exponent: 0.5, // Prestige currency exponent
+    gainMult() { // Calculate the multiplier for main currency from bonuses
+        mult = new Decimal(1)
+        return mult
+    },
+    gainExp() { // Calculate the exponent on main currency from bonuses
+        return new Decimal(1)
+    },
+    row: 2, // Row the layer is in on the tree (0 is the first row)
+    hotkeys: [
+        {key: "y", description: "Y: Reset for gamma", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
+    ],
+    layerShown(){return true},
+	upgrades: {
+		11: {
+			title: "NICE",
+    		description: "x15",
+    		cost: new Decimal(2),
+        },
+		12: {
+			title: "KOLAK",
+    		description: "15x10",
+    		cost: new Decimal(30),
+        },
     },
 })
