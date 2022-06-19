58ac09599524c707926f79dfc3a20fb459ca2343
Annontations6 <106895624+Annontations6@users.noreply.github.com>
2022-06-19 09:39:58 -0300

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -1,14 +1,14 @@
-addLayer("p", {
-    name: "prestige", // This is optional, only used in a few places, If absent it just uses the layer id.
-    symbol: "p", // This appears on the layer's node. Default is the id with the first letter capitalized
+addLayer("K", {
+    name: "kongulus", // This is optional, only used in a few places, If absent it just uses the layer id.
+    symbol: "k", // This appears on the layer's node. Default is the id with the first letter capitalized
     position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
     startData() { return {
         unlocked: true,
 		points: new Decimal(10),
     }},
-    color: "#4BDC13",
-    requires: new Decimal(10), // Can be a function that takes requirement increases into account
-    resource: "prestige points", // Name of prestige currency
+    color: "#FF0000",
+    requires: new Decimal(100), // Can be a function that takes requirement increases into account
+    resource: "kongulus points", // Name of prestige currency
     baseResource: "points", // Name of resource prestige is based on
     baseAmount() {return player.points}, // Get the current amount of baseResource
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
@@ -22,7 +22,14 @@ addLayer("p", {
     },
     row: 0, // Row the layer is in on the tree (0 is the first row)
     hotkeys: [
-        {key: "p", description: "P: Reset for prestige points", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
+        {key: "k", description: "K: Reset for kongulus points", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
     ],
-    layerShown(){return true}
+    layerShown(){return true},
+	upgrades: {
+		11: {
+			title: "get Gain",
+    		description: "mulit by 2.",
+    		cost: new Decimal(2),
+        },
+    },
 })
