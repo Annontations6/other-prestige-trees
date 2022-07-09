af4e95f28010b9e906ed1023fc3636cbd6c0dbea
Throngjwk <105552423+Throngjwk@users.noreply.github.com>
2022-07-09 16:57:06 -0300

    branches

--- js/layers.js
+++ js/layers.js
@@ -201,6 +201,7 @@ addLayer("zz", {
     name: "zig zag", // This is optional, only used in a few places, If absent it just uses the layer id.
     symbol: "ZZ", // This appears on the layer's node. Default is the id with the first letter capitalized
     position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
+    branches: ['c'],
     startData() { return {
         unlocked: true,
 		points: new Decimal(0),
