b2aa34b9f22b891489bf756812975fce7ce76637
Annontations6 <106895624+Annontations6@users.noreply.github.com>
2022-07-23 17:56:20 -0300

    fix aledary mu

--- js/layers.js
+++ js/layers.js
@@ -38,7 +38,7 @@ addLayer("p", {
     },
     automate() {
         if (player.p.x.gte(1)) {
-            player.p.points = player.p.points.times(new Decimal(Math.E).pow(player.p.b.times(1000).times(player.p.x.times(player.p.dt))))
+            player.p.points = player.p.points.times(new Decimal(Math.E).pow(player.p.b.times(1000).times(player.p.x.times(player.p.dt)).mul(new Decimal.pow(10, player.mu.points))))
         }
 
         //x
@@ -87,6 +87,57 @@ addLayer("p", {
 
                 return l;
             }
+        },
+    }
+})
+
+addLayer("mu", {
+    name: "mu", // This is optional, only used in a few places, If absent it just uses the layer id.
+    symbol: "µ", // This appears on the layer's node. Default is the id with the first letter capitalized
+    position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
+    startData() { return {
+        unlocked: true,
+		points: new Decimal(0),
+        dmu:new Decimal(0)
+    }},
+    color: "#555555",
+    requires: new Decimal(10), // Can be a function that takes requirement increases into account
+    resource: "mu", // Name of prestige currency
+    baseResource: "prestige points", // Name of resource prestige is based on
+    baseAmount() {return player.points}, // Get the current amount of baseResource
+    type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
+    exponent: 0.5, // Prestige currency exponent
+    gainMult() { // Calculate the multiplier for main currency from bonuses
+        mult = new Decimal(1)
+        return mult
+    },
+    gainExp() { // Calculate the exponent on main currency from bonuses
+        return new Decimal(1)
+    },
+    row: 1, // Row the layer is in on the tree (0 is the first row)
+    layerShown(){return player.p.gte("1e800000")},
+    clickables: {
+        11: {
+            display() {return "<h2>Prestige at +" + dmu  +" Mu.</h2>"},
+            canClick(){return player.mu.dmu.gte(1)},
+            onClick() {
+                player.p.points = new Decimal(1.0001)
+                player.mu.points = player.mu.points.add(player.mu.dmu)
+            }
+        }
+    },
+    automate(){
+        player.mu.dmu = player.p.points.log10().div(36)
+    },
+    tabFormat: {
+        "Main tab": {
+            content: [
+                "main-display",
+                ["display-text",
+        function() { return 'You Have ' + player.mu.dmu + "dµ, at prestige gain to be reset." },
+        { "color": "yellow", "font-size": "30px", "font-family": "Consolas" }],
+                "clickables"
+            ],
         },
     }
 })
